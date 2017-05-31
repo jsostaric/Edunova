179d83734acc7fc9db984c9f4a9067404e4a913a
Jurica Šoštarić <bugificiran@gmail.com>
2017-05-31 13:57:47 +0200

    Add files via upload

---
+++ dz2/index.php
@@ -0,0 +1,10 @@
+<html>
+	<body>
+		<p>
+			unesi p1 i p2 parametre, gdje p1 je tekst, a p2 boja teksta
+		</p>
+
+		<h1 style="color: <?php echo $_GET['p2']; ?> "><?php echo $_GET['p1']; ?>
+		</h1>
+	</body>
+</html>
